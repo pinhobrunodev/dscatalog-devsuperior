699bc49bd9863970b0f49607c10216b087c043b3
pinhobrunodev <iaishowcs@gmail.com>
2021-08-19 19:56:17 -0300

    Mapeamento Objeto Relacional , Db Seed

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -16,6 +16,8 @@ public class User {
     private String email;
     private String password;
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
